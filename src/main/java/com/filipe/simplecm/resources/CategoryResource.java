cd35b41ef3875ad0641c6ed06f7d9151b815010a
Filipe <fisapo@gmail.com>
2018-08-22 23:20:52 -0400

    Add domain package

--- src/main/java/com/filipe/simplecm/resources/CategoryResource.java
+++ src/main/java/com/filipe/simplecm/resources/CategoryResource.java
@@ -1,16 +1,27 @@
 package com.filipe.simplecm.resources;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.filipe.simplecm.domain.Category;
 
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
+	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST is Working";
+	public List<Category> listing() {
+		Category c1 = new Category(1, "IT");
+		Category c2 = new Category(2, "Office");
+		List<Category> list = new ArrayList<>();
+		list.add(c1);
+		list.add(c2);
+		return list;
 	}
 }
