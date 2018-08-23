10c7d20d67847ce82e27c5667928187dcb7f9e27
Filipe Pompeu <filipepompeu@MacBook-Pro-de-Filipe.local>
2018-08-22 22:39:31 -0400

    Project Created

---
+++ src/main/java/com/filipe/simplecm/SimplecmApplication.java
@@ -0,0 +1,12 @@
+package com.filipe.simplecm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplecmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplecmApplication.class, args);
+	}
+}
